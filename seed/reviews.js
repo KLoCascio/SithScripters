3bf68c54ce63c5dfec2dff6c89df3dba17f3619d
Robert <rcastor0000@gmail.com>
2023-10-10 21:26:38 -0500

    Robert Castor

--- seed/reviews.js
+++ seed/reviews.js
@@ -3,54 +3,57 @@ const {Review, Drink} = require('../models')
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
-
+    const ww = Drink.find({name: 'Wookiee Wake-Up'})
+    const tf = Drink.find({name: 'The Force'})
+    const ss = Drink.find({name: 'Sith Surge'})
+    const jj = Drink.find({name: 'Jedi Jolt'})
 
 
 const reviews = [
     {
-        drinkName: '1',
+        drinkName: ss[0]._id,
         score: 4,
         recommend: true,
         comment: 'This drink was amazing! Highly recommend.'
     },
     {
-        drinkName: '2',
+        drinkName: tf[0]._id,
         score: 2,
         recommend: false,
         comment: 'Not a fan of this drink. Would not recommend.'
     },
     {
-        drinkName: '3',
+        drinkName: ww[0]._id,
         score: 5,
         recommend: true,
         comment: 'The best drink I have ever had. 10/10 would recommend!'
     },
     {
-        drinkName: '3',
+        drinkName: ss[0]._id,
         score: 3,
         recommend: true,
         comment: 'Decent drink, but not exceptional.'
     },
     {
-        drinkName: '4',
+        drinkName: ww[0]._id,
         score: 5,
         recommend: true,
         comment: 'Absolutely loved this drink!'
     },
     {
-        drinkName: '5',
+        drinkName: ss[0]._id,
         score: 1,
         recommend: false,
         comment: 'Terrible drink, would never have it again.'
     },
     {
-        drinkName: '6',
+        drinkName: jj[0]._id,
         score: 4,
         recommend: true,
         comment: 'Great taste and presentation.'
     },
     {
-        drinkName: '7',
+        drinkName: tf[0]._id,
         score: 4,
         recommend: true,
         comment: 'Good choice if you like strong flavors.'
